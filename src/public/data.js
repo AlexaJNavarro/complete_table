92912e2de2af771e4c826ee9d5894f33a106c039
Alexandra Jaqueline Navarro Navarro <alexa@pop-os.localdomain>
2022-01-12 19:09:15 -0500

    consumiendo api de update

--- src/public/data.js
+++ src/public/data.js
@@ -6,41 +6,59 @@ let number_two = document.getElementById("number_two");
 let total = document.getElementById("number_total");
 
 let API_URL = "http://localhost:3008/data";
-const xhr = new XMLHttpRequest();
+// const xhr = new XMLHttpRequest();
 
-//ACTUALIZAR NUEVOS CAMPOS
-let formData = new FormData();
-formData.append("number", number.value);
-formData.append("number_one", number_one.value);
-formData.append("number_two", number_two.value);
-formData.append(
-  "total",
-  Number(number.value) + Number(number_one.value) + Number(number_two.value)
-);
+// //CONSUMIENDO ENDPOINT <<GET ALL DATA>>
+// function onRequestHandler() {
+//   if (this.readyState === 4 && this.status === 200) {
+//     const data = JSON.parse(this.response);
+//     number.value = data.data[0].number;
+//     number_one.value = data.data[0].number_one;
+//     number_two.value = data.data[0].number_two;
+//     total.value = data.data[0].total;
+//   }
+// }
+// xhr.addEventListener("load", onRequestHandler);
+// xhr.open("GET", `${API_URL}/61df0911749dc286aad4c66a`);
+// xhr.send();
 
-//VALIDAR RESPUESTA Y LLENAR DATA EN SUS RESPECTIVOS CAMPOS
-function onRequestHandler() {
-  if (this.readyState === 4 && this.status === 200) {
-    const data = JSON.parse(this.response);
-    number.value = data.data[0].number;
-    number_one.value = data.data[0].number_one;
-    number_two.value = data.data[0].number_two;
-    total.value = data.data[0].total;
-  }
+//CONSUMIENDO ENDPOINT <<UPDATE DATA>>
+function update() {
+  var body = {
+    number: number.value,
+    number_one: number_one.value,
+    number_two: number_two.value,
+    total:
+      Number(number.value) +
+      Number(number_one.value) +
+      Number(number_two.value),
+  };
+
+  console.log("body");
+  console.log(body);
+  axios({
+    method: "PUT",
+    url: `${API_URL}/61df0911749dc286aad4c66a`,
+    data: body,
+  })
+    .then((res) => {
+      console.log("UPDATE");
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log("ERROR");
+      console.log(err);
+    });
 }
 
-//REFRESCAR PAGINA Y ENVIAR QUERY
-xhr.addEventListener("load", onRequestHandler);
-xhr.open("GET", `${API_URL}/61df0911749dc286aad4c66a`);
-xhr.send();
-
 //SOCKET IO
-number.addEventListener("keyup", () => {
+number.addEventListener("keyup", async () => {
   socket.emit("chat:everybody", {
     number: number.value,
     number_one: number_one.value,
     number_two: number_two.value,
   });
+  update();
 });
 
 number_one.addEventListener("keyup", () => {
@@ -49,6 +67,7 @@ number_one.addEventListener("keyup", () => {
     number_one: number_one.value,
     number_two: number_two.value,
   });
+  update();
 });
 
 number_two.addEventListener("keyup", () => {
@@ -57,6 +76,7 @@ number_two.addEventListener("keyup", () => {
     number_one: number_one.value,
     number_two: number_two.value,
   });
+  update();
 });
 
 socket.on("chat:everybody", (data) => {
